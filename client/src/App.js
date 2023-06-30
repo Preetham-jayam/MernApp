d168aab3fa34e6881b432d2c6d09f18ccce530be
Preetham-Jayam <preethamjayam2004@gmail.com>
2023-06-30 19:58:02 +0530

    Client side code added intiated with home page product page

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,19 @@
 import React from 'react'
-
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { Outlet } from 'react-router-dom';
 const App = () => {
   return (
-    <div>App</div>
+    <>
+    <Header />
+    <main className='py-3'>
+      <Container>
+          <Outlet/>
+      </Container>
+    </main>
+    <Footer/>
+    </>
   )
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+// import 'bootstrap/dist/css/bootstrap.min.css'
+import {createBrowserRouter,createRoutesFromElements,Route,RouterProvider} from 'react-router-dom'
+import './assets/styles/index.css';
+import './assets/styles/bootstrap.custom.css';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import HomePage from './pages/HomePage';
+import ProductPage from './pages/ProductPage';
 
+const router=createBrowserRouter(
+  createRoutesFromElements(
+  <Route path='/' element={<App/>}>
+    <Route index={true} path='/' element={<HomePage/>}/>
+    <Route path='/product/:id' element={<ProductPage/>}/>
+  </Route>
+));
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+   <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
